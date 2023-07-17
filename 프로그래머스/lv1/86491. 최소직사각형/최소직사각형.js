0478079dbe700eaca2515674cde1f974ed836f4a
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-07-17 20:27:39 +0900

    [level 1] Title: 최소직사각형, Time: 5.79 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,11 +1,12 @@
 function solution(sizes) {
-    sizes = sizes.map(([w, h]) => w < h ? [h, w] : [w, h])
+    sizes= sizes.map(([w,h]) => w < h ? [h,w] : [w,h])
     
-    let max = [-Infinity, -Infinity]
+    let w = -Infinity
+    let h = -Infinity
+    
     for(let size of sizes) {
-        if(size[0] > max[0]) max[0] = size[0]
-        if(size[1] > max[1]) max[1] = size[1]
+        if(size[0] > w) w = size[0]
+        if(size[1] > h) h = size[1]
     }
-    
-    return max[0] * max[1]
+    return w * h
 }
