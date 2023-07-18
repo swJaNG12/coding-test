7d0ed5009120db48b9250059088d9cd164f28055
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-07-18 18:19:01 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,3 +1,4 @@
 function solution(arr) {
-    return arr.length !== 1 ? arr.filter(el => el !== Math.min(...arr)) : [-1]
+    const min = Math.min(...arr)
+    return arr.length !== 1 ? arr.filter(el => el !== min) : [-1]
 }
