5cce3068b3a3ab57e07248970c9e1f563f26ddcf
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-07-29 17:02:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.91 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,16 @@
+function solution(par, com) {
+    let parObj = createObj(par)
+    let comObj = createObj(com)
+
+    for(const person in parObj) {
+        if(parObj[person] !== comObj[person]) return person
+    }
+}
+
+function createObj(arr) {
+    return obj = arr.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1
+        return acc
+    }, {})
+}
+
