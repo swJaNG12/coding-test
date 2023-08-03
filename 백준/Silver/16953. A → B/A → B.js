89da471ef3152ff361ab033dcc429823b24225da
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-03 16:20:46 +0900

    [Silver II] Title: A → B, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().split('\n');
+
+let [a,b] = input[0].split(' ').map(el => parseInt(el));
+let cnt = 0;
+
+while(b > a) {
+  if(b % 2 === 0) {
+    b /= 2;
+    cnt++;
+  } else if(b % 10 === 1) {
+    b = Math.floor(b/10);
+    cnt++;
+  } else {
+      break;
+  }
+}
+
+a === b ? console.log(cnt+1) : console.log(-1)
