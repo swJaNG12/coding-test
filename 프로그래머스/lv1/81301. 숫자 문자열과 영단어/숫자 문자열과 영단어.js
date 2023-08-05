e8cd9ef1b67329a9a7ae3dda820981cdf4bbe8ec
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-05 14:19:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,6 @@
+function solution(s) {
+    let numbers =  ['zero','one','two','three','four','five','six','seven','eight','nine'];
+    numbers.forEach((el, i) => s = s.replaceAll(el, i.toString()));
+    
+    return parseInt(s)
+}
