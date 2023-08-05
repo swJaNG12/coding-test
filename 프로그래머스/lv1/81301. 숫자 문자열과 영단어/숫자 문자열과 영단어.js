e42b690b4636355e5ef86fa5ee5d1fbaa486787f
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-05 14:24:14 +0900

    Update 숫자 문자열과 영단어.js

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -2,5 +2,5 @@ function solution(s) {
     let numbers =  ['zero','one','two','three','four','five','six','seven','eight','nine'];
     numbers.forEach((el, i) => s = s.replaceAll(el, i.toString()));
     
-    return parseInt(s)
+    return parseInt(s);
 }
