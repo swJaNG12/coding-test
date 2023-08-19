a30eff7b4375da49167ebfe70b3c6667d90eb997
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-19 11:45:33 +0900

    [Silver IV] Title: 수 찾기, Time: 288 ms, Memory: 38320 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -1,25 +1,5 @@
 const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
-const arrN = input[1].split(' ').map(Number).sort((a,b) => a-b);
-const arrM = input[3].split(' ').map(Number);
-let answer = [];
+let set = new Set(input[1].split(' '));
+let answer = input[3].split(' ').map(v => set.has(v) ? 1 : 0).join('\n');
 
-for(let i = 0; i < arrM.length; i++) {
-    let t = arrM[i];
-    let left = 0;
-    let right = arrN.length-1;
-    
-    while(left <= right) {
-        let mid = parseInt((left+right) / 2);
-        
-        if(arrN[mid] > t) right = mid - 1;
-        else if(arrN[mid] < t) left = mid + 1;
-        else if(arrN[mid] === t) {
-            answer.push(1);
-            break;
-        }
-        
-        if(left > right) answer.push(0)
-    }
-}
-
-console.log(answer.join('\n'));
+console.log(answer);
