bf1650d16f72b9409a2cede58dce5a97639162a9
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-20 17:47:18 +0900

    [level 3] Title: 입국심사, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.js
@@ -0,0 +1,14 @@
+function solution(n, times) {
+    let left = 1;
+    let right = Number.MAX_SAFE_INTEGER - 1;
+    while(left <= right) {
+        let mid = parseInt((left+right)/2);
+        const cnt = times.reduce((a, c) => {
+            return a += parseInt(mid / c);
+        }, 0)
+
+        if(cnt < n) left = mid + 1;
+        else right = mid - 1;
+    }
+    return left;
+}
