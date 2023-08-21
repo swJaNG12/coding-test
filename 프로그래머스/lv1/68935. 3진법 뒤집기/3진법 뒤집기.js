a1ef0ac08c852f1cd416254a7bfb967b9992f8b2
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-21 16:23:31 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    const arr = []
-    const num = 3
-    
-    while(n !== 0) {
-        arr.push(n % num)
-        n = Math.floor(n / num)
-    }
-    arr.reverse()
-    
-    return arr.reduce((acc, cur, idx) => {
-        return Math.pow(num, idx) * cur + acc
-    }, 0)
+    return parseInt(n.toString(3).split('').reverse().join(''), 3);
 }
