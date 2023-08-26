81b16a10ccc9fc145b775795f3086d63ae8b60c8
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-26 15:02:29 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,15 +1,3 @@
 function solution(name, yearning, photo) {
-    const map = name.reduce((a,c, i) => {
-        return a.set(c, yearning[i]);
-    }, new Map());
-
-    const answer = [];
-    photo.forEach(persons => {
-       let sum = 0;
-        persons.forEach(person => {
-            sum += map.get(person) || 0;
-        })
-        answer.push(sum);
-    })
-    return answer;
+    return photo.map(v => v.reduce((a,c) => a += yearning[name.indexOf(c)] ?? 0 ,0))
 }
