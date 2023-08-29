18690c4cb1eec5d3c07adbad1d75c56a56f41843
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-08-29 16:29:22 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,9 @@
 function solution(arr, commands) {
-    let answer = []
-    for(let idx = 0; idx < commands.length; idx++) {
-        let [i,j,k] = commands[idx]
-        let result = arr.slice(i-1, j).sort((a,b) => a-b)[k-1]
-        answer.push(result)
+    let answer = [];
+    for(const command of commands) {
+        const [i,j,k] = command;
+        const num = arr.slice(i - 1, j).sort((a,b) => a-b)[k-1]
+        answer.push(num);
     }
-    return answer    
+    return answer;
 }
