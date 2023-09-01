5c35e703743741a308b87a74489b4b2894015765
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-09-01 22:41:55 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,4 +1,10 @@
 function solution(k, score) {
-    var answer = [];
-    return answer;
+    let arr = [];
+    
+    return score.reduce((a,c) => {
+        arr.push(c);
+        arr = arr.sort((a,b) => b-a).slice(0, k);
+        a.push(arr.at(-1));
+        return a;
+    }, [])
 }
