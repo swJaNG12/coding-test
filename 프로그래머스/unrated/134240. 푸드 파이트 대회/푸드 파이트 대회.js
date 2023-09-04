ef0f8d313222429d41d473a9c63062b6ac234180
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-09-04 15:05:25 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,4 +1,4 @@
 function solution (food) {
-  let half = food.map(n => ~~(n/2)).map((n,i) => i.toString().repeat(n)).join('');
+  const half = food.map(n => ~~(n/2)).map((n,i) => i.toString().repeat(n)).join('');
   return half + '0' + half.split('').reverse().join('')
 }
