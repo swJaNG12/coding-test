400bfc355f0fb84850eeb44f0f63d52f3dae223e
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-09-06 22:08:04 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,8 @@
+function solution(cards1, cards2, goal) {
+    for(const word of goal) {
+        if(word === cards1[0]) cards1.shift();
+        else if(word === cards2[0]) cards2.shift();
+        else return 'No';
+    }
+    return 'Yes';
+}
