2a119b05f038700cd2403983275c61f5c589d08f
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-09-10 13:21:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -4,12 +4,10 @@ function solution(nums) {
     for(let j = i+1; j < nums.length-1; j++) {
       for(let k =j+1; k < nums.length; k++) {
         const num = nums[i] + nums[j] + nums[k];
-        const sqrt = ~~Math.sqrt(num);
+          
         let flag = true;
-        for(let l = 2; l <= sqrt; l++) {
-           if(num % l === 0) {
-             flag = false;
-           }
+        for(let l = 2; l <= Math.sqrt(num); l++) {
+           if(num % l === 0) flag = false;
         }
         if(flag) cnt++;
       }
