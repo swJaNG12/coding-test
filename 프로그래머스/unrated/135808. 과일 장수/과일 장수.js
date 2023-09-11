09b24a441f8d4905fb5596b76d6f40c81548af94
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-09-11 20:37:23 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,10 @@
 function solution(k,m,score) {
-    score = score.sort((a,b) => a-b);
-    let sum = 0;
+  score = score.sort((a,b) => a-b);
+  let sum = 0;
 
-    while(score.length >= m) {
-        const box = score.splice(score.length-m);
-        sum += Math.min(...box) * m;
-    }
-    return sum;
+  while(score.length >= m) {
+    const box = score.splice(score.length-m);
+    sum += Math.min(...box) * m;
+  }
+  return sum;
 }
