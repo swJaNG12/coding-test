b5ee66296a19791b3c4280d8a891ed12cedc1090
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-10-13 01:42:06 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.58 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const arr = [];
+    for(let i = 0; i < s.length; i++) {
+        if(arr[arr.length - 1] === s[i]) arr.pop(); 
+        else arr.push(s[i]);
+    } 
+    return !arr.length ? 1 : 0;
+}
